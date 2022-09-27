beb18420951a6deef3872ec6aa7bc0ff53579727
Tenzin992 <93698648+Tenzin992@users.noreply.github.com>
2022-09-26 23:27:44 -0700

    Create lastTwo.java

---
+++ String-1/lastTwo.java
@@ -0,0 +1,30 @@
+/*
+Given a string of any length, return a new string where the last 2 chars, if present, are swapped, so "coding" yields "codign".
+
+
+lastTwo("coding") → "codign"
+lastTwo("cat") → "cta"
+lastTwo("ab") → "ba"
+*/
+public String lastTwo(String str) {
+  if(str.length()==2){
+    return str.substring(str.length()-1) + str.substring(0,1);
+  }else if (str.length()>2){
+    return str.substring(0,str.length()-2) + str.substring(str.length()-1) + str.substring(str.length()-2,str.length()-1);
+  }
+  return str;
+  
+  
+}
+/*
+public String lastTwo(String str){
+  int l = str.length();
+  if(l>=2){
+    return str.substring(0,l-2) + str.charAt(l-1) + str.charAt(l-2);
+  }
+  return str;
+  
+}
+
+
+*/
