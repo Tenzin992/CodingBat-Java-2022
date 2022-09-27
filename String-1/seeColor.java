d9e8621511209420ca2dc389d3a2236e30a7ee6e
Tenzin992 <93698648+Tenzin992@users.noreply.github.com>
2022-09-26 23:28:40 -0700

    Create seeColor.java

---
+++ String-1/seeColor.java
@@ -0,0 +1,20 @@
+/*
+
+Given a string, if the string begins with "red" or "blue" return that color string, otherwise return the empty string.
+
+
+seeColor("redxx") → "red"
+seeColor("xxred") → ""
+seeColor("blueTimes") → "blue"
+*/
+public String seeColor(String str) {
+  if(str.length()<=3 && !str.equals("red")){
+    return "";
+  }else if (str.substring(0,3).equals("red")){
+    return "red";
+  }else if (str.substring(0,4).equals("blue")){
+    return "blue";
+  }else {
+    return "";
+  }
+}
